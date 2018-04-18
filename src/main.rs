2e33ad232d57de354948f155e0263ed43f3fd709
James Waples <jamwaffles@gmail.com>
2018-04-18 22:20:11 +0100

    Initial commit, draw pixel at (0, 0)

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+
+extern crate cortex_m;
+extern crate embedded_hal as hal;
+extern crate panic_abort;
+extern crate ssd1306;
+extern crate stm32f103xx_hal as blue_pill;
+
+use blue_pill::i2c::{DutyCycle, I2c, Mode};
+use blue_pill::prelude::*;
+use ssd1306::prelude::*;
+use ssd1306::Builder;
+
+fn main() {
+    let dp = blue_pill::stm32f103xx::Peripherals::take().unwrap();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = I2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000,
+            duty_cycle: DutyCycle::Ratio1to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    disp.set_pixel(0, 0, 1);
+
+    disp.flush().unwrap();
+}
